dccb51706e57f7d3dea665d77983ab0924b33fb2
bizoon <r.aureli@protonmail.com>
2021-10-27 16:02:38 +0100

    Scaffolding view home (partilas: header, footer) + route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('title', 'Homepage')
+
+@section('content')
+    <h1>Sono il contenuto principale della Homepage</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>Sono l'Header</h1>
+    <ul>
+        <li><a href="{{ route('pagina-home')}}">Home</a></li>
+        <li><a href="{{ route('pagina-about')}}">About</a></li>
+        <li><a href="{{ route('pagina-contact')}}">Contact</a></li>
+    </ul>
+</header>
